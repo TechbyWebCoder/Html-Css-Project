ed7ad6afb732c5c01c5ece6f5f4c343918a0f890
TechbyWebCoder <techbywedcoder@gmail.com>
2024-08-09 17:46:17 +0530

    Create menu.js

---
+++ Animated Menu Bar/menu.js
@@ -0,0 +1,5 @@
+let menuToggle = document.querySelector(".menuToggle");
+      let menu = document.querySelector(".menu");
+      menuToggle.onclick = function () {
+        menu.classList.toggle("active");
+      };
