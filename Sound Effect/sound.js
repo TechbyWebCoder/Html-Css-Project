de82d7b227174d1e61386e548b3f64504f226d01
TechbyWebCoder <techbywedcoder@gmail.com>
2025-03-31 17:50:40 +0530

    Add files via upload

---
+++ Sound Effect/sound.js
@@ -0,0 +1,25 @@
+const sounds = ['classic', 'dog', 'rocket', 'flock', 'intro', 'retro']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
